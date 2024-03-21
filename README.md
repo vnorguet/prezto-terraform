d508b55851cf814a30aad2fc52fa5da3a20da31d
Vincent Norguet <vincent@neutronium.tech>
2024-03-21 19:52:09 +0100

    Add terraform completion and aliases

---
+++ README.md
@@ -0,0 +1,22 @@
+# prezto-terraform
+
+Handy module for Terraform
+
+## Features
+
+* Completion
+* Aliases
+
+## Setup
+
+1. Clone this repository
+2. Add to `${ZDOTDIR:-$HOME}"/.zprezto/contrib`
+3. Enable into `~/.zpreztorc`
+
+    ```shell
+    zstyle ':prezto:load' pmodule \
+      ... \
+      terraform
+    ```
+
+4. Reload your shell
---
+++ init.zsh
@@ -0,0 +1,21 @@
+#
+# Provides terraform aliases and functions.
+#
+# Authors:
+#   Vincent Norguet <vincent.norguet@gmail.com>
+#
+
+# Return if requirements are not found.
+if (( ! $+commands[terraform] )); then
+  return 1
+fi
+
+# Load dependencies.
+pmodload 'helper'
+
+# Source module files.
+source "${0:h}/alias.zsh"
+
+autoload -U +X bashcompinit && bashcompinit
+autoload -Uz compinit && compinit
+complete -o nospace -C /usr/local/bin/terraform terraform
